8e4a21510adffb3dd4d9d5504c5546e71e337442
Jayson Martinez <jaysmartinez@gmail.com>
2023-03-25 15:52:12 -0500

    Update Api Key

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -9,103 +9,85 @@ const router = express.Router();
 router.post('/text', async (req, res) => {
 	try {
 		const { text, activeChatId } = req.body;
+		console.log('text:', text);
 
-		const response = await openai.createChatCompletion({
-			model: 'gpt-3.5-turbo',
-			messages: [
-				{ role: 'system', content: 'You are a helpful assistant.' }, // this represents the bot and what role they will assume
-				{ role: 'user', content: text }, // the message that the user sends
+		// 		const response = await openai.createCompletion({
+		// 			model: 'text-davinci-003',
+		// 			prompt: text,
+		// 			temperature: 0.5,
+		// 			max_tokens: 2048,
+		// 			top_p: 1,
+		// 			frequency_penalty: 0.5,
+		// 			presence_penalty: 0,
+		// 		});
 
-				// BONUS NOTE: you can also provide a list of messages to the bot to give context
-				// and the bot can use that information to respond to the user as needed, ie adding:
-				// { role: "assistant", content: "The weather sucks today." },
+		// 		await axios.post(
+		// 			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
+		// 			{ text: response.data.choices[0].text },
+		// 			{
+		// 				headers: {
+		// 					'Project-ID': process.env.PROJECT_ID,
+		// 					'User-Name': process.env.BOT_USER_NAME,
+		// 					'User-Secret': process.env.BOT_USER_SECRET,
+		// 				},
+		// 			},
+		// 		);
 
-				// to the above messages array, and then asking it this question:
-				// `how is the weather today?`
+		// 		res.status(200).json({ text: response.data.choices[0].text });
+		// 	} catch (error) {
+		// 		console.error('error', error);
+		// 		res.status(500).json({ error: error.message });
+		// 	}
+		// });
 
-				// the bot gave me this response:
-				// `I apologize for my previous response. As an AI language model, I should not use such language.
-				// I do not have access to real-time weather information without your location. Could you please
-				// let me know your location, so I can provide you with accurate weather information?`
+		// router.post('/code', async (req, res) => {
+		// 	try {
+		// 		const { text, activeChatId } = req.body;
 
-				// Hence, if you wanted to keep the "threads" that exist on ChatGPT, you would have to save the
-				// messages that the bot sends and then provide them to the bot in the next request.
-			],
-		});
+		// 		const response = await openai.createCompletion({
+		// 			model: 'code-davinci-002',
+		// 			prompt: text,
+		// 			temperature: 0.5,
+		// 			max_tokens: 2048,
+		// 			top_p: 1,
+		// 			frequency_penalty: 0.5,
+		// 			presence_penalty: 0,
+		// 		});
 
-		await axios.post(
-			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
-			{ text: response.data.choices[0].message.content },
-			{
-				headers: {
-					'Project-ID': process.env.PROJECT_ID,
-					'User-Name': process.env.BOT_USER_NAME,
-					'User-Secret': process.env.BOT_USER_SECRET,
-				},
-			},
-		);
+		// 		await axios.post(
+		// 			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
+		// 			{ text: response.data.choices[0].text },
+		// 			{
+		// 				headers: {
+		// 					'Project-ID': process.env.PROJECT_ID,
+		// 					'User-Name': process.env.BOT_USER_NAME,
+		// 					'User-Secret': process.env.BOT_USER_SECRET,
+		// 				},
+		// 			},
+		// 		);
 
-		res.status(200).json({ text: response.data.choices[0].message.content });
-	} catch (error) {
-		console.error('error', error.response.data.error);
-		res.status(500).json({ error: error.message });
-	}
-});
+		// 		res.status(200).json({ text: response.data.choices[0].text });
+		// 	} catch (error) {
+		// 		console.error('error', error.response.data.error);
+		// 		res.status(500).json({ error: error.message });
+		// 	}
+		// });
 
-router.post('/code', async (req, res) => {
-	try {
-		const { text, activeChatId } = req.body;
-		console.log('🚀 ~ file: openai.js:58 ~ router.post ~ req.body:', req.body);
+		// router.post('/assist', async (req, res) => {
+		// 	try {
+		// 		const { text } = req.body;
 
-		const response = await openai.createChatCompletion({
-			model: 'gpt-3.5-turbo',
-			messages: [
-				{
-					role: 'system',
-					content: 'You are an assistant coder who responds with only code and no explanations.',
-				}, // this represents the bot and what role they will assume
-				{ role: 'user', content: text }, // the message that the user sends
-			],
-		});
+		// 		const response = await openai.createCompletion({
+		// 			model: 'text-davinci-003',
+		// 			prompt: `Finish my thought: ${text}`,
+		// 			temperature: 0.5,
+		// 			max_tokens: 1024,
+		// 			top_p: 1,
+		// 			frequency_penalty: 0.5,
+		// 			presence_penalty: 0,
+		// 		});
 
-		console.log('response.data', response.data);
-
-		await axios.post(
-			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
-			{ text: response.data.choices[0].message.content },
-			{
-				headers: {
-					'Project-ID': process.env.PROJECT_ID,
-					'User-Name': process.env.BOT_USER_NAME,
-					'User-Secret': process.env.BOT_USER_SECRET,
-				},
-			},
-		);
-
-		res.status(200).json({ text: response.data.choices[0].message.content });
-	} catch (error) {
-		console.error('error', error.response.data.error);
-		res.status(500).json({ error: error.message });
-	}
-});
-
-router.post('/assist', async (req, res) => {
-	try {
-		const { text } = req.body;
-
-		const response = await openai.createChatCompletion({
-			model: 'gpt-3.5-turbo',
-			messages: [
-				{
-					role: 'system',
-					content:
-						"You are a helpful assistant that serves to only complete user's thoughts or sentences.",
-				}, // this represents the bot and what role they will assume
-				{ role: 'user', content: `Finish my thought: ${text}` }, // the message that the user sends
-			],
-		});
-
-		res.status(200).json({ text: response.data.choices[0].message.content });
+		res.status(200).json({ text /*: response.data.choices[0].text */ });
 	} catch (error) {
 		console.error('error', error);
 		res.status(500).json({ error: error.message });
