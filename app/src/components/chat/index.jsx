855adc325d1cf5c5a34b4fde684253694ee8c2fd
Jayson Martinez <jaysmartinez@gmail.com>
2023-03-27 19:38:56 -0500

    Resolved API issue and added more features

--- app/src/components/chat/index.jsx
+++ app/src/components/chat/index.jsx
@@ -3,6 +3,8 @@ import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-
 import Header from '@/components/header';
 import StandardMessageForm from '@/components/messageForms/StandardMessageForms';
 import Ai from '@/components/messageForms/Ai';
+import AiCode from '@/components/messageForms/AiCode';
+import AiAssist from '@/components/messageForms/AiAssist';
 
 const Chat = () => {
 	const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID, 'tester', '1234');
@@ -18,6 +20,13 @@ const Chat = () => {
 					if (chatProps.chat?.title.startsWith('AiChat_')) {
 						return <Ai props={props} activeChat={chatProps.chat} />;
 					}
+					if (chatProps.chat?.title.startsWith('AiCode_')) {
+						return <AiCode props={props} activeChat={chatProps.chat} />;
+					}
+					if (chatProps.chat?.title.startsWith('AiAssist_')) {
+						return <AiAssist props={props} activeChat={chatProps.chat} />;
+					}
+
 					return <StandardMessageForm props={props} activeChat={chatProps.chat} />;
 				}}
 			/>
--- server/routes/openai.js
+++ server/routes/openai.js
@@ -9,85 +9,84 @@ const router = express.Router();
 router.post('/text', async (req, res) => {
 	try {
 		const { text, activeChatId } = req.body;
-		console.log('text:', text);
 
-		// 		const response = await openai.createCompletion({
-		// 			model: 'text-davinci-003',
-		// 			prompt: text,
-		// 			temperature: 0.5,
-		// 			max_tokens: 2048,
-		// 			top_p: 1,
-		// 			frequency_penalty: 0.5,
-		// 			presence_penalty: 0,
-		// 		});
+		const response = await openai.createCompletion({
+			model: 'text-davinci-003',
+			prompt: text,
+			temperature: 0.5,
+			max_tokens: 2048,
+			top_p: 1,
+			frequency_penalty: 0.5,
+			presence_penalty: 0,
+		});
 
-		// 		await axios.post(
-		// 			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
-		// 			{ text: response.data.choices[0].text },
-		// 			{
-		// 				headers: {
-		// 					'Project-ID': process.env.PROJECT_ID,
-		// 					'User-Name': process.env.BOT_USER_NAME,
-		// 					'User-Secret': process.env.BOT_USER_SECRET,
-		// 				},
-		// 			},
-		// 		);
+		await axios.post(
+			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
+			{ text: response.data.choices[0].text },
+			{
+				headers: {
+					'Project-ID': process.env.PROJECT_ID,
+					'User-Name': process.env.BOT_USER_NAME,
+					'User-Secret': process.env.BOT_USER_SECRET,
+				},
+			},
+		);
 
-		// 		res.status(200).json({ text: response.data.choices[0].text });
-		// 	} catch (error) {
-		// 		console.error('error', error);
-		// 		res.status(500).json({ error: error.message });
-		// 	}
-		// });
+		res.status(200).json({ text: response.data.choices[0].text });
+	} catch (error) {
+		console.error('error', error);
+		res.status(500).json({ error: error.message });
+	}
+});
 
-		// router.post('/code', async (req, res) => {
-		// 	try {
-		// 		const { text, activeChatId } = req.body;
+router.post('/code', async (req, res) => {
+	try {
+		const { text, activeChatId } = req.body;
 
-		// 		const response = await openai.createCompletion({
-		// 			model: 'code-davinci-002',
-		// 			prompt: text,
-		// 			temperature: 0.5,
-		// 			max_tokens: 2048,
-		// 			top_p: 1,
-		// 			frequency_penalty: 0.5,
-		// 			presence_penalty: 0,
-		// 		});
+		const response = await openai.createCompletion({
+			model: 'code-davinci-002',
+			prompt: text,
+			temperature: 0.5,
+			max_tokens: 2048,
+			top_p: 1,
+			frequency_penalty: 0.5,
+			presence_penalty: 0,
+		});
 
-		// 		await axios.post(
-		// 			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
-		// 			{ text: response.data.choices[0].text },
-		// 			{
-		// 				headers: {
-		// 					'Project-ID': process.env.PROJECT_ID,
-		// 					'User-Name': process.env.BOT_USER_NAME,
-		// 					'User-Secret': process.env.BOT_USER_SECRET,
-		// 				},
-		// 			},
-		// 		);
+		await axios.post(
+			`https://api.chatengine.io/chats/${activeChatId}/messages/`,
+			{ text: response.data.choices[0].text },
+			{
+				headers: {
+					'Project-ID': process.env.PROJECT_ID,
+					'User-Name': process.env.BOT_USER_NAME,
+					'User-Secret': process.env.BOT_USER_SECRET,
+				},
+			},
+		);
 
-		// 		res.status(200).json({ text: response.data.choices[0].text });
-		// 	} catch (error) {
-		// 		console.error('error', error.response.data.error);
-		// 		res.status(500).json({ error: error.message });
-		// 	}
-		// });
+		res.status(200).json({ text: response.data.choices[0].text });
+	} catch (error) {
+		console.error('error', error.response.data.error);
+		res.status(500).json({ error: error.message });
+	}
+});
 
-		// router.post('/assist', async (req, res) => {
-		// 	try {
-		// 		const { text } = req.body;
+router.post('/assist', async (req, res) => {
+	try {
+		const { text } = req.body;
 
-		// 		const response = await openai.createCompletion({
-		// 			model: 'text-davinci-003',
-		// 			prompt: `Finish my thought: ${text}`,
-		// 			temperature: 0.5,
-		// 			max_tokens: 1024,
-		// 			top_p: 1,
-		// 			frequency_penalty: 0.5,
-		// 			presence_penalty: 0,
-		// 		});
+		const response = await openai.createCompletion({
+			model: 'text-davinci-003',
+			prompt: `Finish my thought: ${text}`,
+			temperature: 0.5,
+			max_tokens: 1024,
+			top_p: 1,
+			frequency_penalty: 0.5,
+			presence_penalty: 0,
+		});
 
-		res.status(200).json({ text /*: response.data.choices[0].text */ });
+		res.status(200).json({ text: response.data.choices[0].text });
 	} catch (error) {
 		console.error('error', error);
 		res.status(500).json({ error: error.message });
